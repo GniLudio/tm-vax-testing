bc12516518e35ac4888b299030148b033ea1da9e
GniLudio <ferdinand.rohlfing@googlemail.com>
2025-05-05 11:15:09 +0200

    Add vaccine model

--- backend/vaccines/admin.py
+++ backend/vaccines/admin.py
@@ -1,1 +1,6 @@
+from django.contrib import admin
+
+from vaccines.models import Vaccine
+
 # Register your models here.
+admin.site.register(Vaccine)
--- backend/vaccines/models.py
+++ backend/vaccines/models.py
@@ -1,1 +1,5 @@
+from django.db import models
 # Create your models here.
+
+class Vaccine(models.Model):
+    name = models.fields.CharField(unique=True)
