507d86c0dc94da1d6a837aa89517ef984fc21d33
GniLudio <ferdinand.rohlfing@googlemail.com>
2025-05-05 11:28:27 +0200

    Add custom user model

--- backend/vaccines/admin.py
+++ backend/vaccines/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from vaccines.models import Vaccine
+from vaccines.models import User, Vaccine
 
 # Register your models here.
+admin.site.register(User)
 admin.site.register(Vaccine)
